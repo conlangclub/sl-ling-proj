2c99c6a950a5e3fad5afe748f477a6592fffd3d0
fechan <56131910+fechan@users.noreply.github.com>
2024-06-02 22:20:02 -0700

    prepare to convert to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'conlangpurple': {
+          DEFAULT: '#91008c',
+        }
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+        }
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    function({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+        }
+      })
+    },
+  ]
+}
